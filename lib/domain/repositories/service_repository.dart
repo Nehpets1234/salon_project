da915d2ddd6cad6fad721321d29722b70cb1ffad
jaysonpardilla <jaysonpardilla2004@gmail.com>
2025-10-02 23:01:31 +0800

    initial commit

--- lib/domain/repositories/service_repository.dart
+++
@@ -1,8 +0,0 @@
-// lib/domain/repositories/service_repository.dart
-import 'package:dartz/dartz.dart';
-import '../entities/service.dart';
-import '../entities/failure.dart';
-
-abstract class ServiceRepository {
-	Future<Either<Failure, List<Service>>> getServices();
-}
